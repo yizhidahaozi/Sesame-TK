ffcc7179a665542942307b3f773d328a0751a08e
byseven <2104898527@qq.com>
2025-08-11 19:02:39 +0800

    fix(ViewAppInfo): 移除拼接版本名的注释代码

--- app/src/main/java/fansirsqi/xposed/sesame/data/ViewAppInfo.kt
+++ app/src/main/java/fansirsqi/xposed/sesame/data/ViewAppInfo.kt
@@ -65,7 +65,7 @@ object ViewAppInfo {
                     MMKVSettingsManager.mmkv.encode("verify", it)
                 }
             appBuildNumber = BuildConfig.VERSION_CODE.toString()
-            appTitle = context.getString(R.string.app_name) //+ BuildConfig.VERSION_NAME
+            appTitle = context.getString(R.string.app_name)
             appBuildTarget = BuildConfig.BUILD_DATE + " " + BuildConfig.BUILD_TIME + " ⏰"
             try {
                 appVersion = "${BuildConfig.VERSION_NAME} " + emojiList.random()
