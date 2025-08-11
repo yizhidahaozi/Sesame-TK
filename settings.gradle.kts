cd90db230d574bb592e4a9bdc7a79e0e1b2fb624
byseven <2104898527@qq.com>
2025-08-11 19:02:34 +0800

    refactor: 调整仓库配置和版本目录

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,10 +10,19 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
+    repositoriesMode = RepositoriesMode.FAIL_ON_PROJECT_REPOS
     repositories {
-        maven("https://maven.aliyun.com/repository/spring")
-        mavenCentral()
         google()
+        mavenCentral()
+        maven("https://maven.aliyun.com/repository/spring")
+        mavenLocal {
+            content {
+                includeGroup("io.github.libxposed")
+            }
+        }
+    }
+    versionCatalogs {
+        create("libs")
     }
 }
 plugins {
