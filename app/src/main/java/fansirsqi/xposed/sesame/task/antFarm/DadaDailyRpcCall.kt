1c5405453b8ca844bdfbe65ee8151111ab51c11b
byseven <2104898527@qq.com>
2025-08-15 18:56:18 +0800

    转为kt

--- app/src/main/java/fansirsqi/xposed/sesame/task/antFarm/DadaDailyRpcCall.kt
+++ app/src/main/java/fansirsqi/xposed/sesame/task/antFarm/DadaDailyRpcCall.kt
@@ -1,19 +1,23 @@
-package fansirsqi.xposed.sesame.task.antFarm;
+package fansirsqi.xposed.sesame.task.antFarm
 
-import fansirsqi.xposed.sesame.hook.RequestManager;
+import fansirsqi.xposed.sesame.hook.RequestManager.requestString
 
 /**
  * @author Constanline
  * @since 2023/08/04
  */
-public class DadaDailyRpcCall {
-    public static String home(String activityId) {
-        return RequestManager.requestString("com.alipay.reading.game.dadaDaily.home",
-                "[{\"activityId\":" + activityId + ",\"dadaVersion\":\"1.3.0\",\"version\":1}]");
+object DadaDailyRpcCall {
+    fun home(activityId: String?): String {
+        return requestString(
+            "com.alipay.reading.game.dadaDaily.home",
+            "[{\"activityId\":$activityId,\"dadaVersion\":\"1.3.0\",\"version\":1}]"
+        )
     }
 
-    public static String submit(String activityId, String answer, Long questionId) {
-        return RequestManager.requestString("com.alipay.reading.game.dadaDaily.submit",
-                "[{\"activityId\":" + activityId + ",\"answer\":\"" + answer + "\",\"dadaVersion\":\"1.3.0\",\"questionId\":" + questionId + ",\"version\":1}]");
+    fun submit(activityId: String?, answer: String?, questionId: Long?): String {
+        return requestString(
+            "com.alipay.reading.game.dadaDaily.submit",
+            "[{\"activityId\":$activityId,\"answer\":\"$answer\",\"dadaVersion\":\"1.3.0\",\"questionId\":$questionId,\"version\":1}]"
+        )
     }
 }
