d99ef134b46db99e769d24ce989556e2129bcd85
yizhidahaozi <167291691+yizhidahaozi@users.noreply.github.com>
2025-08-16 18:25:32 +0800

    Delete app directory

--- app/src/main/java/fansirsqi/xposed/sesame/hook/rpc/intervallimit/DefaultIntervalLimit.kt
+++
@@ -1,5 +0,0 @@
-package fansirsqi.xposed.sesame.hook.rpc.intervallimit
-
-class DefaultIntervalLimit(override val interval: Int?) : IntervalLimit {
-    override var time: Long = 0
-}
--- app/src/main/java/fansirsqi/xposed/sesame/hook/server/handlers/RpcRequest.kt
+++
@@ -1,6 +0,0 @@
-package fansirsqi.xposed.sesame.hook.server.handlers
-
-data class RpcRequest(
-    val methodName: String,
-    val requestData: String
-)
