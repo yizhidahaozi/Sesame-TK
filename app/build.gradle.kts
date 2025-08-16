7a2d09e9816b5ebae0b7baae6e1ca0b83f7427c5
yizhidahaozi <167291691+yizhidahaozi@users.noreply.github.com>
2025-08-16 18:40:26 +0800

    Add files via upload

---
+++ app/build.gradle.kts
@@ -0,0 +1,239 @@
+import java.text.SimpleDateFormat
+import java.util.*
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "fansirsqi.xposed.sesame"
+    compileSdk = 36
+
+    defaultConfig {
+        vectorDrawables.useSupportLibrary = true
+        applicationId = "fansirsqi.xposed.sesame"
+        minSdk = 21
+        targetSdk = 36
+
+        if (!System.getenv("CI").toBoolean()) {
+            ndk {
+                abiFilters.addAll(listOf("armeabi-v7a", "arm64-v8a", "x86", "x86_64"))
+            }
+        }
+
+        // 版本配置
+        val major = 0
+        val minor = 2
+        val patch = 6
+        val buildTag = "alpha"
+
+        val buildDate = SimpleDateFormat("yyyy-MM-dd", Locale.CHINA).apply {
+            timeZone = TimeZone.getTimeZone("GMT+8")
+        }.format(Date())
+
+        val buildTime = SimpleDateFormat("HH:mm:ss", Locale.CHINA).apply {
+            timeZone = TimeZone.getTimeZone("GMT+8")
+        }.format(Date())
+
+        val buildTargetCode = try {
+            buildDate.replace("-", ".") + "." + buildTime.replace(":", ".")
+        } catch (_: Exception) {
+            "0000"
+        }
+
+        val gitCommitCount = try {
+            val process = Runtime.getRuntime().exec(arrayOf("git", "rev-list", "--count", "HEAD"))
+            val output = process.inputStream.bufferedReader().use { it.readText() }.trim()
+            process.waitFor()
+            if (process.exitValue() == 0) {
+                output.toInt()
+            } else {
+                val error = process.errorStream.bufferedReader().use { it.readText() }
+                println("Git error: $error")
+                "1".toInt()
+            }
+        } catch (_: Exception) {
+            "1".toInt()
+        }
+
+
+        versionCode = gitCommitCount
+        versionName = if (buildTag.contains("alpha") || buildTag.contains("beta")) {
+            "v$major.$minor.$patch-$buildTag.$buildTargetCode"
+        } else {
+            "v$major.$minor.$patch-$buildTag"
+        }
+
+        buildConfigField("String", "BUILD_DATE", "\"$buildDate\"")
+        buildConfigField("String", "BUILD_TIME", "\"$buildTime\"")
+        buildConfigField("String", "BUILD_NUMBER", "\"$buildTargetCode\"")
+        buildConfigField("String", "BUILD_TAG", "\"$buildTag\"")
+        buildConfigField("String", "VERSION", "\"v$major.$minor.$patch\"")
+
+        ndk {
+            abiFilters.addAll(listOf("armeabi-v7a", "arm64-v8a", "x86", "x86_64"))
+        }
+
+        testOptions {
+            unitTests.all {
+                it.enabled = false
+            }
+        }
+    }
+
+    buildFeatures {
+        buildConfig = true
+        compose = true
+    }
+
+
+    flavorDimensions += "default"
+    productFlavors {
+        create("normal") {
+            dimension = "default"
+            extra.set("applicationType", "Normal")
+        }
+        create("compatible") {
+            dimension = "default"
+            extra.set("applicationType", "Compatible")
+        }
+    }
+    compileOptions {
+        // 全局默认设置
+        isCoreLibraryDesugaringEnabled = true // 启用脱糖
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlin {
+        compilerOptions {
+            jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+        }
+    }
+
+    productFlavors.all {
+        when (name) {
+            "normal" -> {
+                compileOptions {
+                    sourceCompatibility = JavaVersion.VERSION_17
+                    targetCompatibility = JavaVersion.VERSION_17
+                }
+                kotlin {
+                    compilerOptions {
+                        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+                    }
+                }
+            }
+
+            "compatible" -> {
+                compileOptions {
+                    sourceCompatibility = JavaVersion.VERSION_11
+                    targetCompatibility = JavaVersion.VERSION_11
+                }
+                kotlin {
+                    compilerOptions {
+                        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
+                    }
+                }
+            }
+        }
+    }
+
+    signingConfigs {
+        getByName("debug") {
+        }
+    }
+
+    buildTypes {
+        getByName("debug") {
+            isDebuggable = true
+            versionNameSuffix = "-debug"
+            isShrinkResources = false
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            signingConfig = signingConfigs.getByName("debug")
+        }
+        getByName("release") {
+            isDebuggable = false
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+
+    sourceSets {
+        getByName("main") {
+            jniLibs.srcDirs("src/main/jniLibs")
+        }
+    }
+    val cmakeFile = file("src/main/cpp/CMakeLists.txt")
+    if (!System.getenv("CI").toBoolean() && cmakeFile.exists()) {
+        externalNativeBuild {
+            cmake {
+                path = cmakeFile
+                version = "3.31.6"
+                ndkVersion = "29.0.13113456"
+            }
+        }
+    }
+
+    applicationVariants.all {
+        val variant = this
+        variant.outputs.all {
+            val flavorName = variant.flavorName.replaceFirstChar { it.uppercase() }
+            val fileName = "Sesame-TK-$flavorName-${variant.versionName}.apk"
+            (this as com.android.build.gradle.internal.api.BaseVariantOutputImpl).outputFileName = fileName
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.ui.tooling.preview.android)
+    val composeBom = platform("androidx.compose:compose-bom:2025.05.00")
+    implementation(composeBom)
+    testImplementation(composeBom)
+    androidTestImplementation(composeBom)
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.5")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+    implementation("androidx.compose.runtime:runtime-livedata")
+    implementation("org.nanohttpd:nanohttpd:2.3.1")
+
+
+    implementation(libs.androidx.constraintlayout)
+
+    implementation(libs.activity.compose)
+
+    implementation(libs.core.ktx)
+    implementation(libs.kotlin.stdlib)
+    implementation(libs.slf4j.api)
+    implementation(libs.logback.android)
+    implementation(libs.appcompat)
+    implementation(libs.recyclerview)
+    implementation(libs.viewpager2)
+    implementation(libs.material)
+    implementation(libs.webkit)
+    compileOnly(libs.xposed.api)
+    compileOnly(libs.lombok)
+    annotationProcessor(libs.lombok)
+    implementation(libs.okhttp)
+    implementation(libs.dexkit)
+
+    coreLibraryDesugaring(libs.desugar)
+
+    add("normalImplementation", libs.jackson.core)
+    add("normalImplementation", libs.jackson.databind)
+    add("normalImplementation", libs.jackson.annotations)
+
+    add("compatibleImplementation", libs.jackson.core.compatible)
+    add("compatibleImplementation", libs.jackson.databind.compatible)
+    add("compatibleImplementation", libs.jackson.annotations.compatible)
+}
--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -1,1 +1,71 @@
+# Add project specific ProGuard rules here.
+# By default, the flags in this file are appended to flags specified
+# in C:\tools\adt-bundle-windows-x86_64-20131030\sdk/tools/proguard/proguard-android.txt
+# You can edit the include path and order by changing the proguardFiles
+# directive in build.gradle.
+#
+# For more details, see
+#   http://developer.android.com/guide/developing/tools/proguard.html
 
+# Add any project specific keep options here:
+
+# If your project uses WebView with JS, uncomment the following
+# and specify the fully qualified class name to the JavaScript interface
+# class:
+#-keepclassmembers class fqcn.of.javascript.interface.for.webview {
+#   public *;
+#}
+
+
+-keep class de.robv.android.xposed.** { *; }
+
+# Keep Logback classes
+-keep class ch.qos.logback.** { *; }
+-keep class org.slf4j.** { *; }
+-dontwarn ch.qos.logback.**
+-dontwarn org.slf4j.**
+
+# Keep your application classes
+-keep class fansirsqi.xposed.sesame.** { *; }
+-keepattributes *Annotation*
+-keepattributes Signature
+-keepattributes Exceptions
+
+# Keep Jackson classes and annotations
+-keep class com.fasterxml.jackson.** { *; }
+-keepattributes Signature
+-keepattributes RuntimeVisibleAnnotations
+-keepattributes RuntimeVisibleParameterAnnotations
+-keepattributes RuntimeVisibleTypeAnnotations
+-keepattributes AnnotationDefault
+
+# Keep Java Beans classes
+-keep class java.beans.** { *; }
+-dontwarn java.beans.**
+
+# Keep Jackson Java7 Support
+-keep class com.fasterxml.jackson.databind.ext.Java7SupportImpl { *; }
+-dontwarn com.fasterxml.jackson.databind.ext.Java7SupportImpl
+
+# Keep ModelField classes and their members
+-keep class fansirsqi.xposed.sesame.model.ModelField { *; }
+-keep class fansirsqi.xposed.sesame.model.modelFieldExt.** { *; }
+-keepclassmembers class * extends fansirsqi.xposed.sesame.model.ModelField {
+    <fields>;
+    <methods>;
+}
+
+# Additional serialization rules
+-keepclassmembers class * {
+    @com.fasterxml.jackson.annotation.* *;
+}
+-keepnames class * implements java.io.Serializable
+-keepclassmembers class * implements java.io.Serializable {
+    static final long serialVersionUID;
+    private static final java.io.ObjectStreamField[] serialPersistentFields;
+    !static !transient <fields>;
+    private void writeObject(java.io.ObjectOutputStream);
+    private void readObject(java.io.ObjectInputStream);
+    java.lang.Object writeReplace();
+    java.lang.Object readResolve();
+}
