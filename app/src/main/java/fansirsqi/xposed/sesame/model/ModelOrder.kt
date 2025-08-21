5534f6f83195c4802464ff1599cc8ba615ff6c13
托尼老师五毛钱洗剪吹 <23072523+ALLG999@users.noreply.github.com>
2025-08-21 10:33:54 +0800

    调整设置界面模块排序

--- app/src/main/java/fansirsqi/xposed/sesame/model/ModelOrder.kt
+++ app/src/main/java/fansirsqi/xposed/sesame/model/ModelOrder.kt
@@ -22,21 +22,21 @@ object ModelOrder {
     private val array = arrayOf(
         BaseModel::class.java,       // 基础设置
         AntForest::class.java,       // 森林
-        AntSports::class.java,       // 运动
         AntFarm::class.java,         // 庄园
         AntOrchard::class.java,    // 农场
         AntOcean::class.java,        // 海洋
-        AntDodo::class.java,       // 神奇物种
-        AncientTree::class.java,     // 古树
-        AntCooperate::class.java,    // 合种
-        Reserve::class.java,       // 保护地
-        AntMember::class.java,     // 会员
         AntStall::class.java,      // 蚂蚁新村
+        AntDodo::class.java,       // 神奇物种
+        AntCooperate::class.java,    // 合种
+        AntSports::class.java,       // 运动
+        AntMember::class.java,     // 会员
+        AncientTree::class.java,     // 古树
         GreenFinance::class.java,  // 绿色经营
+        Reserve::class.java,       // 保护地
+        AnswerAI::class.java         // AI答题
       //AntBookRead::class.java,   // 读书
       //ConsumeGold::class.java,   // 消费金
       //OmegakoiTown::class.java,  // 小镇
-        AnswerAI::class.java         // AI答题
     )
 
     val allConfig: List<Class<out Model>> = array.toList()
