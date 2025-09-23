daeb892e1d719f62c1fe0a0b5a295705b78655f6
托尼老师五毛钱洗剪吹 <23072523+ALLG999@users.noreply.github.com>
2025-09-23 22:17:02 +0800

    调整import

--- app/src/main/java/fansirsqi/xposed/sesame/newui/DeviceInfo.kt
+++ app/src/main/java/fansirsqi/xposed/sesame/newui/DeviceInfo.kt
@@ -1,13 +1,15 @@
 package fansirsqi.xposed.sesame.newui
 
+import android.annotation.SuppressLint
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
 import android.os.Build
-import android.widget.Toast
+import android.provider.Settings
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.combinedClickable
+import android.widget.Toast
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -22,10 +24,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.PreviewParameterProvider
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import fansirsqi.xposed.sesame.BuildConfig
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 
 class PreviewDeviceInfoProvider : PreviewParameterProvider<Map<String, String>> {
     override val values: Sequence<Map<String, String>> = sequenceOf(
