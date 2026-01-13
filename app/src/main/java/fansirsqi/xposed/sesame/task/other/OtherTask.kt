3565ae2fdd1b6d3b43641f0a21dc85717ee150a4
teakone <113753847+teakone@users.noreply.github.com>
2026-01-13 15:07:12 +0800

    优化 信用2101 小游戏
    添加了可选次数的游戏选项

--- app/src/main/java/fansirsqi/xposed/sesame/task/other/OtherTask.kt
+++ app/src/main/java/fansirsqi/xposed/sesame/task/other/OtherTask.kt
@@ -1,8 +1,11 @@
 package fansirsqi.xposed.sesame.task.other
 
+import fansirsqi.xposed.sesame.entity.OtherEntityProvider.listCreditOptions
 import fansirsqi.xposed.sesame.model.ModelFields
 import fansirsqi.xposed.sesame.model.ModelGroup
 import fansirsqi.xposed.sesame.model.modelFieldExt.BooleanModelField
+import fansirsqi.xposed.sesame.model.modelFieldExt.SelectAndCountModelField
+import fansirsqi.xposed.sesame.model.modelFieldExt.SelectModelField
 import fansirsqi.xposed.sesame.task.ModelTask
 import fansirsqi.xposed.sesame.task.other.credit2101.Credit2101
 import fansirsqi.xposed.sesame.util.Log
@@ -21,20 +24,59 @@ class OtherTask : ModelTask() {
         return ""
     }
 
+    /** @brief 信用2101 游戏开关 */
     private var credit2101: BooleanModelField? = null
+
+    /** @brief 信用2101 事件列表 */
+    private var CreditOptions: SelectAndCountModelField? = null
+
+
+    /** @brief 信用2101 自动开宝箱 */
+    private var AutoOpenChest: BooleanModelField? = null
+
+    /** @brief 信用2101 仅完成1次的事件列表 */
+    private var CreditOnceOptions: SelectModelField? = null
+
+
     override fun getFields(): ModelFields {
         val fields = ModelFields()
         fields.addField(
             BooleanModelField(
                 "credit2101", "信用2101", false
             ).apply { credit2101 = this })
+
+        fields.addField(
+            BooleanModelField(
+                "AutoOpenChest", "信用2101 | 自动开宝箱", false
+            ).apply { AutoOpenChest = this })
+
+
+        fields.addField(
+            SelectAndCountModelField(
+                "CreditOptions",
+                "信用2101 | 事件类型",
+                LinkedHashMap<String?, Int?>(),
+                listCreditOptions(),
+                "设置运行次数(-1为不限制)"
+            ).also {
+                CreditOptions = it
+            })
+
+
+
+
+
+
+
+
+
         return fields
     }
 
     override suspend fun runSuspend() {
         try {
             if (credit2101!!.value) {
-                Credit2101.doCredit2101()
+                Credit2101.doCredit2101(AutoOpenChest!!.value==true,CreditOptions!!)
             }
         } catch (e: Exception) {
             Log.printStackTrace(TAG, e)
