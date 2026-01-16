ac4938d3488f0f5f7c9624a22ea00aec3ac30583
byseven <fansirliu@foxmail.com>
2026-01-16 11:11:03 +0800

    chore(app): 更新应用版本号从 0.9.3 到 0.9.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,7 @@ android {
         }.format(Date())
 
         versionCode = gitCommitCount
-        versionName = "0.9.3"
+        versionName = "0.9.7"
 
         buildConfigField("String", "BUILD_DATE", "\"$buildDate\"")
         buildConfigField("String", "BUILD_TIME", "\"$buildTime\"")
