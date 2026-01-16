b8cd56dea0805d4b8d72f15652272144df22ed06
byseven <fansirliu@foxmail.com>
2026-01-16 17:05:46 +0800

    refactor(other): 重构信用2101模块代码结构
    - 移除冗余的 record 导入
    - 将变量名 CreditOptions 更改为 creditOptions
    - 将变量名 AutoOpenChest 更改为 autoOpenChest
    - 将变量名 CreditOnceOptions 更改为 creditOnceOptions
    - 更新字段初始化逻辑以匹配新的变量命名
    - 调整 Credit2101 调用以使用新的变量命名

--- app/src/main/java/fansirsqi/xposed/sesame/task/other/OtherTask.kt
+++ app/src/main/java/fansirsqi/xposed/sesame/task/other/OtherTask.kt
@@ -9,7 +9,6 @@ import fansirsqi.xposed.sesame.model.modelFieldExt.SelectModelField
 import fansirsqi.xposed.sesame.task.ModelTask
 import fansirsqi.xposed.sesame.task.other.credit2101.Credit2101
 import fansirsqi.xposed.sesame.util.Log
-import fansirsqi.xposed.sesame.util.Log.record
 
 class OtherTask : ModelTask() {
     override fun getName(): String {
@@ -28,14 +27,14 @@ class OtherTask : ModelTask() {
     private var credit2101: BooleanModelField? = null
 
     /** @brief 信用2101 事件列表 */
-    private var CreditOptions: SelectAndCountModelField? = null
+    private var creditOptions: SelectAndCountModelField? = null
 
 
     /** @brief 信用2101 自动开宝箱 */
-    private var AutoOpenChest: BooleanModelField? = null
+    private var autoOpenChest: BooleanModelField? = null
 
     /** @brief 信用2101 仅完成1次的事件列表 */
-    private var CreditOnceOptions: SelectModelField? = null
+    private var creditOnceOptions: SelectModelField? = null
 
 
     override fun getFields(): ModelFields {
@@ -48,7 +47,7 @@ class OtherTask : ModelTask() {
         fields.addField(
             BooleanModelField(
                 "AutoOpenChest", "信用2101 | 自动开宝箱", false
-            ).apply { AutoOpenChest = this })
+            ).apply { autoOpenChest = this })
 
 
         fields.addField(
@@ -59,7 +58,7 @@ class OtherTask : ModelTask() {
                 listCreditOptions(),
                 "设置运行次数(-1为不限制)"
             ).also {
-                CreditOptions = it
+                creditOptions = it
             })
 
 
@@ -76,7 +75,7 @@ class OtherTask : ModelTask() {
     override suspend fun runSuspend() {
         try {
             if (credit2101!!.value) {
-                Credit2101.doCredit2101(AutoOpenChest!!.value==true,CreditOptions!!)
+                Credit2101.doCredit2101(autoOpenChest!!.value==true,creditOptions!!)
             }
         } catch (e: Exception) {
             Log.printStackTrace(TAG, e)
