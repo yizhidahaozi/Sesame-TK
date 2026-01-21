4d62b7efc2e71e623a06c444c77a964c0febc292
byseven <fansirliu@foxmail.com>
2026-01-21 15:54:03 +0800

    chore(release): 更新版本号到 0.9.8
    - 将 versionName 从 0.9.7 更新为 0.9.8
    - 保留了构建日期和时间的自动生成配置

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,7 @@ android {
         }.format(Date())
 
         versionCode = gitCommitCount
-        versionName = "0.9.7"
+        versionName = "0.9.8"
 
         buildConfigField("String", "BUILD_DATE", "\"$buildDate\"")
         buildConfigField("String", "BUILD_TIME", "\"$buildTime\"")
