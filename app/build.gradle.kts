e74fa04d909273c1954e194e7668f35020a2d2f5
byseven <fansirliu@foxmail.com>
2026-01-23 18:29:30 +0800

    chore(release): 更新版本号从 0.9.8 到 0.9.9
    - 将应用版本号从 0.9.8 升级到 0.9.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,7 @@ android {
         }.format(Date())
 
         versionCode = gitCommitCount
-        versionName = "0.9.8"
+        versionName = "0.9.9"
 
         buildConfigField("String", "BUILD_DATE", "\"$buildDate\"")
         buildConfigField("String", "BUILD_TIME", "\"$buildTime\"")
